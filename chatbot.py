24553ffcd0f7f515a6f0a88b3b2a6114d53feb84
Ashwith Poojary <pashwith25@gmail.com>
2021-10-15 11:18:44 +0530

    Created basic chatbot

---
+++ chatbot.py
@@ -0,0 +1,63 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.h5')
+
+def cleanSentence(sentence):
+    sentence = sentence.lower()
+    sentence = nltk.word_tokenize(sentence)
+    sentence = [lemmatizer.lemmatize(word) for word in sentence]
+    return sentence
+
+def bagOfWords(sentence):
+    sentence = cleanSentence(sentence)
+    bag = [0]*len(words)
+    for s in sentence:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict(sentence):
+    bag = bagOfWords(sentence)
+    res = model.predict(np.array([bag]))[0]
+    results = [[i,r] for i,r in enumerate(res) if r>0.25]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+
+    return return_list
+
+def response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = i
+            break
+
+    return result
+
+print("CovidBOT is active")
+
+while True:
+    sentence = input("You: ")
+    results = predict(sentence)
+    resp = response(results, intents)
+    print("CovidBOT: ", random.choice(resp["responses"]))
+    if resp["tag"] == "goodbye":
+        break
---
+++ training.py
@@ -0,0 +1,76 @@
+#imports
+
+import random
+import json
+import pickle
+import numpy as np
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.optimizers import SGD
+
+import nltk
+# nltk.download('punkt')
+# nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_chars = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        documents.append((w, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_chars]
+words = sorted(list(set(words)))
+
+classes = sorted(list(set(classes)))
+
+#saving the 2 words files
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+output = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    pattern_words = document[0]
+    pattern_words = [lemmatizer.lemmatize(w.lower()) for w in pattern_words]
+
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+    
+    output_row = list(output)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_X = list(training[:,0])
+train_Y = list(training[:,1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_X[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_Y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+hist = model.fit(np.array(train_X), np.array(train_Y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+print("Training completed!")
